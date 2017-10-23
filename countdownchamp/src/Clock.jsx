cdada1cc3015c9131e472fa48e13c44e4e434676
Anthony <anthonyguitarman13@gmail.com>
2017-10-23 16:56:27 -0600

    live countdown working

--- countdownchamp/src/Clock.jsx
+++ countdownchamp/src/Clock.jsx
@@ -9,18 +9,35 @@ class Clock extends Component {
             hour: 0,
             minutes: 0,
             seconds: 0
-
         }
+    }
+
+    componentWillMount() {
+        this.getTimeUntil(this.props.deadline)
+    }
+
+    componentDidMount() {
+        setInterval(() => this.getTimeUntil(this.props.deadline), 1000);
+    }
+
+    getTimeUntil(deadline) {
+        const time = Date.parse(deadline) - Date.parse(new Date());
+        const seconds = Math.floor((time/1000) % 60);
+        const minutes = Math.floor((time/1000/60) % 60);
+        const hours =  Math.floor(time/(1000*60*60) % 24);
+        const days = Math.floor(time/(1000*60*60*24));
+
+        this.setState({days, hours, minutes, seconds});
     }
 
     render() {
         return (
             <div>
-            <div className="Clock-days">{this.state.days} Days</div>
-            <div className="Clock-hours">{this.state.hours} Hours</div>
-            <div className="Clock-minutes">{this.state.minutes} Minutes</div>
-            <div className="Clock-seconds">{this.state.seconds} Seconds</div>
-       </div>
+                <div className="Clock-days">{this.state.days} Days</div>
+                <div className="Clock-hours">{this.state.hours} Hours</div>
+                <div className="Clock-minutes">{this.state.minutes} Minutes</div>
+                <div className="Clock-seconds">{this.state.seconds} Seconds</div>
+            </div>
         )
     }
 }
