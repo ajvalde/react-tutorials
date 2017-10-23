19b81ca0b535424ba0cb1844a47a7053733b39df
Anthony <anthonyguitarman13@gmail.com>
2017-10-23 14:09:49 -0600

    Gave clock inline display

--- countdownchamp/src/App.jsx
+++ countdownchamp/src/App.jsx
@@ -5,12 +5,12 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <div>Countdown to December 25, 2017</div>
+                <div className="App-title">Countdown to December 25, 2017</div>
                 <div>
-                    <div>14 Days</div>
-                    <div>30 Hours</div>
-                    <div>15 Minutes</div>
-                    <div>20 Seconds</div>
+                    <div className="Clock-days">14 Days</div>
+                    <div className="Clock-hours">30 Hours</div>
+                    <div className="Clock-minutes">15 Minutes</div>
+                    <div className="Clock-seconds">20 Seconds</div>
                 </div>
                 <div>
                     <input placeholder='new date' />
