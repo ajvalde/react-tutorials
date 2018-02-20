b219e338a527a2e4df0a4bd177b0aefa1f52503e
Anthony <anthonyguitarman13@gmail.com>
2018-02-19 17:45:59 -0700

    added switch

--- router-demo/src/App.js
+++ router-demo/src/App.js
@@ -25,28 +25,28 @@ const Products = () => (
 )
 
 /* App component */
-class App extends React.Component {
+
+class App extends Component {
   render() {
+
     return (
       <div>
         <nav className="navbar navbar-light">
           <ul className="nav navbar-nav">
-
-           /* Link components are used for linking to other views */
             <li><Link to="/">Homes</Link></li>
             <li><Link to="/category">Category</Link></li>
             <li><Link to="/products">Products</Link></li>
-
           </ul>
-         </nav>
+       </nav>
 
-          /* Route components are rendered if the path prop matches the current URL */
-           <Route path="/" component={Home}/>
-           <Route path="/category" component={Category}/>
-           <Route path="/products" component={Products}/>
+    <Switch>
+      <Route exact path="/" component={Home}/>
+      <Route path="/category" component={Category}/>
+       <Route path="/products" component={Products}/>
+    </Switch>
 
-      </div>
-    )
+    </div>
+    );
   }
 }
 
