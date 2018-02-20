4bb535034b014bb709bfd6d21a0919d2c2fe6e43
Anthony <anthonyguitarman13@gmail.com>
2018-02-19 18:31:48 -0700

    added product

--- router-demo/src/Products.jsx
+++ router-demo/src/Products.jsx
@@ -1,93 +1,75 @@
 import React from 'react';
 import { Link, Route } from 'react-router-dom';
-
-
-
+import  Product  from './Product';
 
 const Products = ({ match }) => {
 
-    const Product = ({match,data}) => {
-        var product= data.find(p => p.id == match.params.productId);
-        var productData;
-      
-        if(product)
-          productData = <div>
-            <h3> {product.name} </h3>
-            <p>{product.description}</p>
-            <hr/>
-            <h4>{product.status}</h4>  </div>;
-        else
-          productData = <h2> Sorry. Product doesnt exist </h2>;
-      
-        return (
-          <div>
-            <div>
-               {productData}
-            </div>
-          </div>
-        )    
-      }
-
     const productsData = [
-        {
-          id: 1,
-          name: 'NIKE Liteforce Blue Sneakers',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin molestie.',
-          status: 'Available'
-        
-        },
-        {
-          id: 2,
-          name: 'Stylised Flip Flops and Slippers',
-          description: 'Mauris finibus, massa eu tempor volutpat, magna dolor euismod dolor.',
-          status: 'Out of Stock'
-        
-        },
-        {
-          id: 3,
-          name: 'ADIDAS Adispree Running Shoes',
-          description: 'Maecenas condimentum porttitor auctor. Maecenas viverra fringilla felis, eu pretium.',
-          status: 'Available'
-        },
-        {
-          id: 4,
-          name: 'ADIDAS Mid Sneakers',
-          description: 'Ut hendrerit venenatis lacus, vel lacinia ipsum fermentum vel. Cras.',
-          status: 'Out of Stock'
-        },
-        
-        ];
-
-
+     {
+         id: 1,
+         name: 'NIKE Liteforce Blue Sneakers',
+         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin molestie.',
+         status: 'Available'
+ 
+     },
+     {
+         id: 2,
+         name: 'U.S. POLO ASSN. Slippers',
+         description: 'Mauris finibus, massa eu tempor volutpat, magna dolor euismod dolor.',
+         status: 'Available'
+ 
+     },
+     {
+         id: 3,
+         name: 'ADIDAS Adispree Running Shoes',
+         description: 'Maecenas condimentum porttitor auctor. Maecenas viverra fringilla felis, eu pretium.',
+         status: 'Available'
+     },
+     {
+         id: 4,
+         name: 'Lee Cooper Mid Sneakers',
+         description: 'Ut hendrerit venenatis lacus, vel lacinia ipsum fermentum vel. Cras.',
+         status: 'Out of Stock'
+     },
+    
+ ];
    var linkList = productsData.map( (product) => {
      return(
-       <li>
+       <li key={product.id}>
          <Link to={`${match.url}/${product.id}`}>
            {product.name}
          </Link>
        </li>
        )
- 
+           
      })
- 
+  
    return(
      <div>
-         <div>
-          <div>
-            <h3> Products</h3>
-            <ul> {linkList} </ul>
-          </div>
-         </div>
+      <div style={{ display: 'block',  justifyContent: "space-between" }}>
+       <div style={{
+         float:'left',
+         padding: '10px',
+         width: '30%',
+         background: '#f0f0f0',
+         marginLeft:'auto' }}>
  
-         <Route path={`${match.url}/:productId`}
-             render={ (props) => <Products data= {productsData} {...props} />}/>
-         <Route exact path={match.url}
-             render={() => (
-             <div>Please select a product.</div>
-             )}
-         />
+         <h3> Products</h3>
+         <ul  style={{ listStyleType: 'none', padding: 0, fontSize:'15px' }}> {linkList} </ul>
+       </div>
      </div>
+     
+     <Route path={`${match.url}/:productId`} render={ (props) => <Product data= {productsData} {...props} />}/>
+     <Route exact path={match.url} 
+       render={() => (
+         <div style={{ textAlign:'center'}}>Please select a product.</div>
+     )}/>
+ 
+     </div>
+ 
    )
  }
+
+
 
 export default Products;
